8b0c876635d81323056c9362afe21da0eb901a04
Henrique da Silva Lima <51914997+henrikesilva@users.noreply.github.com>
2020-01-14 13:50:37 -0300

    Criação Lab 01

---
+++ CSharp/Laboratorios/Lab01/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab01
+{
+    partial class lab01Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultadoLabel = new System.Windows.Forms.Label();
+            this.valorTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.calcularButton.Location = new System.Drawing.Point(118, 23);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(75, 23);
+            this.calcularButton.TabIndex = 1;
+            this.calcularButton.Text = "Calcular";
+            this.calcularButton.UseVisualStyleBackColor = true;
+            this.calcularButton.Click += new System.EventHandler(this.calcularButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite um Número";
+            // 
+            // resultadoLabel
+            // 
+            this.resultadoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultadoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultadoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultadoLabel.Location = new System.Drawing.Point(12, 61);
+            this.resultadoLabel.Name = "resultadoLabel";
+            this.resultadoLabel.Size = new System.Drawing.Size(181, 46);
+            this.resultadoLabel.TabIndex = 2;
+            this.resultadoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // valorTextBox
+            // 
+            this.valorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.valorTextBox.Location = new System.Drawing.Point(12, 25);
+            this.valorTextBox.Name = "valorTextBox";
+            this.valorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.valorTextBox.TabIndex = 0;
+            this.valorTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lab01Form
+            // 
+            this.AcceptButton = this.calcularButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.valorTextBox);
+            this.Controls.Add(this.resultadoLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calcularButton);
+            this.Name = "lab01Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laboratório";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultadoLabel;
+        private System.Windows.Forms.TextBox valorTextBox;
+    }
+}
+
---
+++ CSharp/Laboratorios/Lab01/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class lab01Form : Form
+    {
+        public lab01Form()
+        {
+            InitializeComponent();
+        }
+
+        private void calcularButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = 0;
+                int quadrado = 0;
+
+                numero = Convert.ToInt32(valorTextBox.Text);
+
+                quadrado = numero * numero;
+
+                resultadoLabel.Text = $"Quadrado de {numero} é: {quadrado.ToString("N0")}";
+
+                valorTextBox.Focus();
+                valorTextBox.SelectAll();
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Favor digitar apenas numeros");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Numero Informado extenso demais para calculo");
+            }
+            finally
+            {
+                valorTextBox.Focus();
+                valorTextBox.SelectAll();
+            }
+        }
+    }
+}
