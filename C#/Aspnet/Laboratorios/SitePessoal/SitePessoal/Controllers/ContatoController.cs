bde5699ebbff7ed3297f09f3a32fca40925dafdf
Henrique da Silva Lima <51914997+henrikesilva@users.noreply.github.com>
2020-01-16 14:57:46 -0300

    Update Page, Model and Controller Contato

--- C#/Aspnet/Laboratorios/SitePessoal/SitePessoal/Controllers/ContatoController.cs
+++ C#/Aspnet/Laboratorios/SitePessoal/SitePessoal/Controllers/ContatoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SitePessoal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,31 @@ namespace SitePessoal.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Concluido = false;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Contato contato)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(contato.Email)
+                    || string.IsNullOrEmpty(contato.Nome)
+                    || string.IsNullOrEmpty(contato.Mensagem))
+                {
+                    throw new ArgumentException("Favor preencher todo o formulário");
+                }
+
+                ViewBag.Concluido = true;
+            }
+            catch(Exception ex)
+            {
+                ViewBag.ErroMsg = ex.Message;
+                ViewBag.Concluido = false;
+            }
+            return View("Index");
         }
     }
 }
