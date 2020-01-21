f910611d602e82873ad76f4c5f5370c41f8a2805
henrikesilva <51914997+henrikesilva@users.noreply.github.com>
2020-01-21 14:32:47 -0300

    Conclusão Laboratorio 7

---
+++ C#/Laboratorios/SiteEmpresarial/SiteEmpresarial/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using SiteEmpresarial.Db;
+using SiteEmpresarial.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteEmpresarial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ContatoDb ObterDbContext()
+        {
+            return new ContatoDb();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QuemSomos()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contato(Contato contato)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    throw new ValidationException("Favor preencher todos os campos obrigatórios");
+                }
+
+                using (var db = ObterDbContext())
+                {
+                    var existeContato = db.Contatos.Where(c => c.Email == contato.Email && c.Nome == contato.Nome).Any();
+
+                    if (existeContato)
+                    {
+                        throw new ValidationException("Contato inserido já cadastrado, tente outras informações");
+                    }
+
+                    db.Contatos.Add(contato);
+                    db.SaveChanges();
+                }
+
+                return View("ContatoOk");
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(contato);
+            }
+        }
+    }
+}
---
+++ C#/Laboratorios/SiteEmpresarial/SiteEmpresarial/Db/ContatoDb.cs
@@ -0,0 +1,21 @@
+﻿using SiteEmpresarial.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SiteEmpresarial.Db
+{
+    public class ContatoDb : DbContext
+    {
+        private static string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\di3906\Desktop\Unidas_BootCamp\C#\Laboratorios\SiteEmpresarial\SiteEmpresarial\App_Data\EmpresaAbcDb.mdf;Integrated Security=True";
+
+        public ContatoDb() : base(conexao)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
